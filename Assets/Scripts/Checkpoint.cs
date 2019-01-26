655f0802c29015a32a25f06923e47d61cea2a291
Ethan Heil <ethan.heil@mymail.champlain.edu>
2019-01-26 01:08:54 -0500

    Adding WIP Checkpoint Scripts

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public CheckpointController checkpointController;
+    public bool isCurrentCP = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkpointController = GameObject.FindGameObjectWithTag("GM").GetComponent<CheckpointController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit Checkpoint");
+            for(int i = 0; i < checkpointController.checkpoints.Length; i++)
+            {
+                if(checkpointController.checkpoints[i] == this.gameObject)
+                {
+                    checkpointController.currentCheckpoint = this.gameObject;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+    public GameObject[] checkpoints;
+    public GameObject currentCheckpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCheckpoint = checkpoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
