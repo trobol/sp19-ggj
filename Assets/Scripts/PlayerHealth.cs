84071aaee354539e3158c296e82873acae2efef7
Chris Foster <christopher.foster@mymail.champlain.edu>
2019-01-26 01:07:42 -0500

    Adding health bar and initial tiling work

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+
+    Animator anim;
+    //PlayerMovement playerMovement
+    public bool damaged = false;
+    bool isDead;
+
+
+
+    void Awake()
+    {
+        currentHealth = startingHealth; 
+        //playerMovement = GetComponent <PlayerMovement> ();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damaged)
+        {
+            damageImage.color = flashColor;
+        }
+        else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        // Set the damaged flag so the screen will flash.
+        damaged = true;
+
+        // Reduce the current health by the damage amount.
+        currentHealth -= amount;
+
+        // Set the health bar's value to the current health.
+        healthSlider.value = currentHealth;
+
+        
+        
+
+        // If the player has lost all it's health and the death flag hasn't been set yet...
+        if (currentHealth <= 0 && !isDead)
+        {
+            // ... it should die.
+            Death();
+        }
+
+
+       
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        anim.SetTrigger("Die");
+
+        //playerMovement.enabled = false;
+    }
+} 
