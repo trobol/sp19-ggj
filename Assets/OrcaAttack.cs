f4716c12b488da7f5d79cb54941bfd026d0a391e
Thornton <thorfernbacher@gmail.com>
2019-01-26 18:40:34 -0500

    orcas
    The orcas move towards the player and when close to player slow down

---
+++ Assets/OrcaAttack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrcaAttack : MonoBehaviour
+{
+    public GameObject player;
+
+    public float orcaSpeed;
+
+    private float timeToSlow = 0;
+
+    public float slowRate = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveTowards(player);
+    }
+
+    void moveTowards(GameObject target)
+    {
+        Vector3 dir = target.transform.position - transform.position;
+        dir = dir.normalized;
+        dir.z = 0;
+        if(Mathf.Abs(Vector3.Distance(target.transform.position, transform.position)) < 4 && Time.time > timeToSlow)
+        {
+            timeToSlow += Time.time + slowRate;
+            orcaSpeed -= 0.01f;
+        }
+        transform.right = target.transform.position - transform.position;
+        transform.position += dir * Time.deltaTime * orcaSpeed;
+    }
+}
