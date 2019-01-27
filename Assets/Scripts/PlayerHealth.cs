11d090b6776390b169f8272bc286b145102cb650
Aidan <thatkidsmurph@gmail.com>
2019-01-26 20:51:17 -0500

    Finalize Damage
    Added Values for health and damage. Orca will knock player back and deal damage when it hits player

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int startingHealth = 100;
+    public int startingHealth = 12;
     public int currentHealth;
     public Slider healthSlider;
     public Image damageImage;
